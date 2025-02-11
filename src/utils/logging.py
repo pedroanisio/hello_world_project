9ebbe18b96dfe783e26980f4fc6bcc5b1607af1f
Pedro Silva <pedroanisio@arc4d3.com>
2025-02-11 06:03:44 +0000

    Update logging and requirements configuration
    - Add log files to .gitignore
    - Include pydantic email validation in requirements
    - Update logging file path to use src/logs directory

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -10,6 +10,6 @@ logger = logging.getLogger("app_logger")
 logger.addHandler(json_handler)
 logger.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler("logs/app.log")
+file_handler = logging.FileHandler("src/logs/app.log")
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
