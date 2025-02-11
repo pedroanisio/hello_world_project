2c46c7f677aef440f0d87333f5c5a088d2b0cfcd
Pedro Silva <pedroanisio@arc4d3.com>
2025-02-11 15:52:55 +0000

    Update pre-commit config and add new exceptions
    - Configure black and isort with consistent formatting settings
    - Add JWT to requirements
    - Introduce new custom exceptions for token and user-related errors

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -3,6 +3,19 @@ class PasswordTooWeakException(Exception):
         self.message = message
         super().__init__(self.message)
 
+
+class InvalidTokenError(Exception):
+    def __init__(self, message="Invalid or expired token"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class UserNotFoundError(Exception):
+    def __init__(self, message="User not found"):
+        self.message = message
+        super().__init__(self.message)
+
+
 class CustomAppException(Exception):
     def __init__(self, message="An application error occurred"):
         self.message = message
